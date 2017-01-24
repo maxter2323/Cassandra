462608b6d91de9bf39d137dfaecfaab134271cd3
maxter2323 <maxter2323@gmail.com>
2017-01-24 06:49:19 +0100

    Light commit

---
+++ Assets/Game/Scripts/ButterflyAnim.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButterflyAnim : MonoBehaviour 
+{
+	private void Start () 
+	{
+		foreach (AnimationState state in GetComponent<Animation>())
+		{
+			state.speed = 0.5f;
+		}
+	}
+}
